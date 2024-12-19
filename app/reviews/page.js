7753136990d69391e5c631ab41416c2dbf4b7a9c
Tony <federicoaguirre134@gmail.com>
2024-12-18 21:54:29 -0600

    made tailwind changes and added more pages

--- app/reviews/page.js
+++ app/reviews/page.js
@@ -20,7 +20,7 @@ function page() {
       <div className=" text-white flex flex-col h-screen justify-center items-center w-screen overflow-hidden">
         <div className="flex flex-col bg-slate-800 p-4 rounded-lg w-fit h-fit m-4 justify-center text-2xl border border-sky-300">
           <br />
-
+            <p className='text-red-600 font-bold'>under construction</p>
         </div>
       </div>
 
