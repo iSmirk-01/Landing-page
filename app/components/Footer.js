7237c67926a470d52153648f704335a3565d0da8
Tony <federicoaguirre134@gmail.com>
2024-12-28 21:56:16 -0600

    made footer fit theme

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -1,15 +1,19 @@
 const Footer = () => {
-    return (
-      <footer className="flex flex-col p-4 bg-DarkThemeMainBlue items-center w-full gap-2 overflow-x-hidden bg-Yellow text-Charcoal mt-8">
-          <p className="">
-            Disclaimer: All custom build services come with a money back
-            guarantee if parts are delivered damaged or defective. Replacements
-            are subject to the supplier the component was purchased from. I
-            &apos;do not&apos; purchase parts for customers.
-          </p>
-          <p>© 2025 Our Services. All rights reserved.</p>
-      </footer>
-    );
-}
+  return (
+    <footer className="flex flex-col items-center text-Charcoal gap-4 mt-8 text-Yellow footerHighlight">
+      <p className="text-center text-sm md:text-base max-w-3xl p-3">
+        Disclaimer: All custom build services come with a money-back guarantee
+        if parts are delivered damaged or defective. Replacements are subject to
+        the supplier from which the component was purchased. I{" "}
+        <span className="font-semibold">do not</span> purchase parts for
+        customers.
+      </p>
+      <hr className="w-full border-t-2 border-Yellow" />
+      <p className="text-center text-xs md:text-sm p-3">
+        © 2025 Our Services. All rights reserved.
+      </p>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
